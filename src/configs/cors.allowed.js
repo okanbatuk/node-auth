cbf14e96bb18ee841e6956ee9a27bf8566cad902
myrn <okanbatuk@protonmail.com>
2023-06-11 21:48:29 +0300

    Added Client origin for cors

--- src/configs/cors.allowed.js
+++ src/configs/cors.allowed.js
@@ -1,3 +1,3 @@
-const allowedOrigins = ["http://localhost:3000", "http://127.0.0.1:5500"];
+const allowedOrigins = ["http://localhost:3000", "http://localhost:5173"];
 
 module.exports = allowedOrigins;
